81a8a1d028b082e7214de7aca3500a432fc734a9
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2020-01-11 12:18:32 +0530

    ADDED: create welcome message api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
